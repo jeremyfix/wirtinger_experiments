d0c7378f9372f47d5ff3223758e58c4d6d341d54
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-04-17 22:28:50 +0200

    adds the printed output

--- ex_complexgrad_torch.py
+++ ex_complexgrad_torch.py
@@ -13,6 +13,7 @@ l = nn.Linear(2, 1, dtype=dtype)
 
 x = torch.randn(16, 2, dtype=dtype)
 
+#
 # First experiment
 # Propagate throught the network, compute the complex output
 # and apply the backward operation
@@ -22,7 +23,9 @@ out.backward()
 
 lwgrad = l.weight.grad
 print(f"Out={out} and dout/dlw*={lwgrad}")
+# This prints : Out=(-0.4594990611076355-0.5016348361968994j) and dout/dlw*=tensor([[ 0.1376+0.2357j, -0.1335-0.0466j ]])
 
+#
 # Second experiment
 # Compute the real part of the output before computing the gradient
 l.zero_grad()
@@ -31,3 +34,4 @@ out.backward()
 
 lwgrad = l.weight.grad
 print(f"Out={out} and dout/dlw*={lwgrad}")
+# This prints : Out=-0.4594990611076355 and dout/dlw*=tensor([[ 0.1376+0.2357j, -0.1335-0.0466j ]])
